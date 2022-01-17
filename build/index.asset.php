a7939975ad582d2dc518d5728fd30626de98648b
Muhammad Muhsin <muhammad.muhseen@gmail.com>
2022-01-17 17:06:48 +0530

    merge changes

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'fcb5a0f011d406bbd9197f86590d5d37');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '992da347656f230057c0168daa398da8');
